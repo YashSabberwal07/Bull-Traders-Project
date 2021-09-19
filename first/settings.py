cd38a16fcf70e3d3d6e13239478049e111131bd0
Yash <yashsabberwal@gmail.com>
2021-09-19 23:05:28 +0530

    web app name

--- first/settings.py
+++ first/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kxb)slgatb!^#i5hec8xn)21xx=!%q^l%r3!)@!$-_8%i1#k-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','bull-traders.herokuapp.com']
 
 
 # Application definition
